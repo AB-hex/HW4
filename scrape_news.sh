b63aa2333bd5e5712f49ef1787969072c9c6244e
AB-hex <74324857+AB-hex@users.noreply.github.com>
2020-12-07 19:28:27 +0200

    Update scrape_news.sh

--- scrape_news.sh
+++ scrape_news.sh
@@ -4,13 +4,9 @@ wget -q https://www.ynetnews.com/category/3082 -O list.txt
 
 grep -o "https://www.ynetnews.com/article/[a-ZA-Z0-9]\{9\}" list.txt > link_list.txt
 
-#cat link_list.txt | uniq | wc -l > results.csv
 uniq link_list.txt  > link_list2.txt
 
-#sort link_list.txt | uniq > link_list2.txt
 cat link_list2.txt | wc -l >results.csv 
-
-#sort link_list.txt | uniq | wc -l > results.csv
 
 for link in `cat link_list2.txt`; do
 	wget -q $link -O tmp.txt
