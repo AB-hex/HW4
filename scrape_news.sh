b95b8165f9365a7ac7333234b6eac772abb99f94
AB-hex <74324857+AB-hex@users.noreply.github.com>
2020-12-11 00:26:52 +0200

    Updated

--- scrape_news.sh
+++ scrape_news.sh
@@ -2,13 +2,15 @@
 
 wget -q https://www.ynetnews.com/category/3082 -O list.txt
 
-grep -o "https://www.ynetnews.com/article/[a-ZA-Z0-9]\{9\}" list.txt > link_list.txt
+grep -o "https://www.ynetnews.com/article/[a-ZA-Z0-9]\{9,\}" list.txt > link_list.txt
 
-uniq link_list.txt  > link_list2.txt
+sort link_list.txt | uniq > uniq.txt
 
-cat link_list2.txt | wc -l >results.csv 
+grep -x '.\{42\}' uniq.txt > filtered.txt
 
-for link in `cat link_list2.txt`; do
+cat filtered.txt | wc -l > results.csv 
+
+for link in `cat filtered.txt`; do
 	wget -q $link -O tmp.txt
 
 	gantz_num=`grep Gantz tmp.txt | wc -l`
