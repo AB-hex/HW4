299cda711b0e7da04b50a0b716c2ff2a7dc2b627
AB-hex <74324857+AB-hex@users.noreply.github.com>
2020-12-15 18:43:13 +0200

    Adding comments to scrape news

--- scrape_news.sh
+++ scrape_news.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 wget -q https://www.ynetnews.com/category/3082 -O list.txt
-
+#find all the addresses of the articles
 grep -o "https://www.ynetnews.com/article/[a-ZA-Z0-9]\{9,\}" list.txt > link_list.txt
 
 sort link_list.txt | uniq > uniq.txt
-
+# chooes the article with the right length
 grep -x '.\{42\}' uniq.txt > filtered.txt
 
 cat filtered.txt | wc -l > results.csv 
-
+# count in evety article the number of Netanyahu and Gantz
 for link in `cat filtered.txt`; do
 	wget -q $link -O tmp.txt
 
@@ -21,6 +21,7 @@ for link in `cat filtered.txt`; do
 		echo '-' >>results.csv
 
 	else
+	# print the results
 		echo 'Netanyahu, '$nehentanyahu_num', Gantz, '$gantz_num >> results.csv 
 
 	fi
